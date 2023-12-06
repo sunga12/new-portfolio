b5a4d7e87f03f610fe765812119ed3cd65cbb61d
Sunga Thawethe <sungabt@gmail.com>
2023-12-06 14:42:26 +0200

    Add categories

--- src/components/Works.js
+++ src/components/Works.js
@@ -6,9 +6,29 @@ import projects from '../projects/projects';
 const Works = () => {
   return (
     <div>
-      <h2>Some of my works</h2>
+      <h2>Front-End Projects</h2>
 
-  {projects.map((project, index) => (
+  {projects.filter((project) => project.category === 'Front-End').map((project, index) => (
+    <Project key={index} 
+      name={project.name}
+      description={project.description}
+      featuredImage={project.featuredImage}
+      technologies={project.technologies} />
+  ))}
+
+<h2>Back-End Projects</h2>
+
+{projects.filter((project) => project.category === 'Back-End').map((project, index) => (
+    <Project key={index} 
+      name={project.name}
+      description={project.description}
+      featuredImage={project.featuredImage}
+      technologies={project.technologies} />
+  ))}
+
+<h2>Full-Stack Projects</h2>
+
+{projects.filter((project) => project.category === 'Full-Stack').map((project, index) => (
     <Project key={index} 
       name={project.name}
       description={project.description}
