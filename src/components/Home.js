b081c49f2a245024a44f9de8e02633f7f1949a88
Sunga Thawethe <sungabt@gmail.com>
2023-12-06 04:18:58 +0200

    Add Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div class="headline">
+      <h1 class="hello-me">Hey, I'm Sunga.</h1>
+      <h3 class="SD">Full-Stack Developer</h3>
+      <p class="info">I can help you build a product, feature or website. Look through some of my work and experience! If you like what you see, don't hesitate to contact me.</p>
+    </div>
+  )
+}
+
+export default Home;
