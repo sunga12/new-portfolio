47050b1f0c2a4fb99ddf512fc29907f1f0421118
Sunga Thawethe <sungabt@gmail.com>
2023-12-06 04:19:39 +0200

    Add About.js

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div class="ab-info">
+      <h2 class="about-title">About me</h2>
+      <p class="about-me">Hello I'm a Full-stack developer! I can help you build a product, feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don't hesitate to contact me.</p>
+      <a class="cv-link" href="https://docs.google.com/document/d/1w6tg-esOivXjZ0izzSLLaS1Tf7EvQyjDWNKVFFEPppk/edit?usp=sharing"><button class="resume-link">Get My Resume</button></a>
+    </div>
+  )
+}
+
+export default About;
