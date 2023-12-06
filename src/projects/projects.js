ed49869fd3859f7d47c56d85c8ff81b48f3b3fa0
Sunga Thawethe <sungabt@gmail.com>
2023-12-06 12:43:17 +0200

    Add projects

---
+++ src/projects/projects.js
@@ -0,0 +1,70 @@
+const Projects = [
+  {
+    name: 'Royal School Website',
+    description: 'School Website Designed using the Cindy Shen Template on Behance. Located in Ndola, Zambia, Royal is proud to offer a supportive and inclusive community that values diversity, creativity, and environmental responsibility. Explore the website and learn more about how Royal Primary & Secondary School can help your child reach their full potential.',
+    featuredImage: './assets/images/rss-square.png',
+    technologies: [['CSS3'], ['HTML5'], ['Javascript'], ['Bootstrap']],
+    liveLink: 'https://sunga12.github.io/Capstone-project-1/',
+    sourceLink: 'https://github.com/sunga12/Capstone-project-1',
+    category: 'Front-End'
+ },
+  {
+    name: 'Country Insights Web-App',
+    description:   'A basic webapp that displays countries and their various details, & allows users to Navigate between the home page and details page, depending on the country of their choice.',
+    featuredImage: './assets/images/CIW.png',
+    technologies: [['React'], ['Redux'], ['CSS3']],
+    liveLink: 'https://country-insights-webapp.onrender.com/',
+    sourceLink: 'https://github.com/sunga12/country-insights-webapp',
+    category: 'Front-End'
+  },
+  {
+    name: 'Space Travellers Hub',
+    description: ' A Group project as Part of the Microverse Full-Stack Curriculum, allowing users to view and make reservations for Rockets, & join and become members of any desired Space Mission.',
+    featuredImage:  './images/Thumbnail SPH.jpg',
+    technologies: [['React'], ['Redux'], ['CSS3']], 
+    liveLink: 'https://ankitt-26k-space-travelers-hub.onrender.com/', 
+    sourceLink:'https://github.com/ankitt26/Space-Travelers-Hub',
+    category: 'Front-End'
+    },
+    {
+    name: 'TV-Show API',
+    description: 'A website built using ES6 that used the fetch API to display information About TV Shows with the capability to add comments, likes, and make reservations (Group Project)',
+    featuredImage:  './images/TVS API Dark.png',
+    technologies: [['HTML5'], ['CSS3'], ['ES6']], 
+    liveLink: 'https://waelelsafty07.github.io/JavaScript-capstone-project/dist/', 
+    sourceLink:'https://github.com/waelelsafty07/JavaScript-capstone-project',
+    category: 'Front-End'
+    },
+    {
+    name: 'Math Magicians',
+    description: 'A Simple SPA that has 3 Sections. A Home page to tell you a little about myself, a Calculator, and a Random Quote generator the gives you a new quote each time you navigate to the quotes page, through a fetch request from the API-Ninja API. ',
+    featuredImage:  './images/MM-Calc.png',
+    technologies: [['React'], ['CSS3'], ['HTML5']], 
+    liveLink: 'https://math-magicians-2hlc.onrender.com', 
+    sourceLink:'https://github.com/sunga12/maths-magic',
+    category: 'Front-End'
+    },
+    {
+    name: 'Bookstore',
+    description: 'A basic website that displays books and their details using an API. It also allows users to Add Books to the Store with a name and author, with the option to remove the book. Each book also has a progress circle that displays the current reading progress.',
+    featuredImage:  './images/BS.png',
+    technologies: [['React'], ['Redux'], ['CSS3']], 
+    liveLink: '#', 
+    sourceLink:'https://github.com/sunga12/bookstore/',
+    category: 'Front-End'
+    },
+    {
+    name: 'Data Dashboard Healthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    featuredImage:  './images/Proj3-bg.png',
+    technologies: [['HTML'], ['Bootstrap'], ['Ruby']], 
+    liveLink: '#', 
+    sourceLink:'#',
+    category: 'Front-End'
+    },
+  
+];
+
+export default Projects;
+
+  
