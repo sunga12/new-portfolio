90731e9f12773ea6b4290dfb8ce000c45586a291
Sunga Thawethe <sungabt@gmail.com>
2023-12-06 14:41:50 +0200

    Update About

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div class="ab-info">
       <h2 class="about-title">About me</h2>
-      <p class="about-me">Hello I'm a Full-stack developer! I can help you build a product, feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don't hesitate to contact me.</p>
+      <p class="about-me">I'm a Full-stack developer! I love music, and mentoring! I can help you build a product, feature or website. Look through some of my work and experience! If you like what you see and have a project you need coded, don't hesitate to contact me.</p>
       <a class="cv-link" href="https://docs.google.com/document/d/1w6tg-esOivXjZ0izzSLLaS1Tf7EvQyjDWNKVFFEPppk/edit?usp=sharing"><button class="resume-link">Get My Resume</button></a>
     </div>
   )
