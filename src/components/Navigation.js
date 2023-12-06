845a377a85264b7a5c2d38c20633b8956396e834
Sunga Thawethe <sungabt@gmail.com>
2023-12-06 04:06:44 +0200

    Add nav file

---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <header>
+      <nav>
+      <ul class="links">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#works">Portfolio</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Navigation;
