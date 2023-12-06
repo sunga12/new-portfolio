2374fa9eb4835c0e6dfa889cfb15034ff4ddefd2
Sunga Thawethe <sungabt@gmail.com>
2023-12-06 12:45:47 +0200

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Navigation from './components/Navigation';
+import Works from './components/Works';
 
 function App() {
   return (
-    <div className="App">
-        <h2>Hello World!</h2>
+    <div>
+        <Navigation />
+        <Home />
+        <Works />
+        <About />
+        <Contact />
     </div>
   );
 }
