aa9cb246abce165fad6dad61431d9ecdd22082b2
Sunga Thawethe <sungabt@gmail.com>
2023-12-06 12:47:10 +0200

    Add projects to works.js

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import Project from './Project';
+import projects from '../projects/projects';
+
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <div>
+      <h2>Some of my works</h2>
+
+  {projects.map((project, index) => (
+    <Project key={index} 
+      name={project.name}
+      description={project.description}
+      featuredImage={project.featuredImage}
+      technologies={project.technologies} />
+  ))}
+    </div>
   )
 }
 
