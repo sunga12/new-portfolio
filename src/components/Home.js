d01807c96b71ea7f1f96effe19d9941a40c2a353
Sunga Thawethe <sungabt@gmail.com>
2023-12-09 06:11:17 +0200

    Update Home text

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,7 @@ const Home = () => (
   <div className="headline">
     <h1 className="hello-me">Hey, I&apos;m Sunga.</h1>
     <h3 className="SD">
-      Full-Stack Developer
-      <br />
-      React | Ruby on Rails | PostgreSQL |
+      A Full-Stack Developer.
     </h3>
     <p className="info">I deliver exceptional user experiences & and robust solutions. Need help? Get in touch.</p>
   </div>
