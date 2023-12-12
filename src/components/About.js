4b12bee3464d235f3438838feb4fb32d4806341d
Sunga Thawethe <sungabt@gmail.com>
2023-12-12 04:22:13 +0200

    Add button animation to about

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
+const buttonVariants = {
+  hover: {
+    scale: 1.05,
+    textShadow: '0px 0px 8px rgb(255,255,255)',
+    boxShadow: '0px 0px 8px rgb(255,255,255)',
+    transition: {
+      repeat: Infinity,
+      repeatType: 'mirror',
+      duration: 0.4,
+    },
+  },
+};
 
 const About = () => (
   <div className="ab-info">
-    <h2 className="about-title">About me</h2>
+    <h1 className="about-title">About me</h1>
     <p className="about-me">I&apos;m a Full-stack developer! I love music, and mentoring! I can help you build a product, feature or website. Look through some of my work and experience! If you like what you see and have a project you need coded, don&apos;t hesitate to contact me.</p>
-    <a className="cv-link" href="https://docs.google.com/document/d/1w6tg-esOivXjZ0izzSLLaS1Tf7EvQyjDWNKVFFEPppk/edit?usp=sharing"><button type="button" className="resume-link">Get My Resume</button></a>
+    <motion.button
+      variants={buttonVariants}
+      whileHover="hover"
+      type="button"
+      className="resume-link"
+      href="https://docs.google.com/document/d/1w6tg-esOivXjZ0izzSLLaS1Tf7EvQyjDWNKVFFEPppk/edit?usp=sharing"
+    >
+      Get My Resume
+    </motion.button>
   </div>
 );
 
