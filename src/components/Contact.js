89d27378585530d5cac9fec94eb71b4994a97ad9
Sunga Thawethe <sungabt@gmail.com>
2023-12-12 04:22:35 +0200

    Add button animation to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
+const buttonVariants = {
+  hover: {
+    scale: 1.05,
+    textShadow: '0px 0px 8px rgb(255,255,255)',
+    boxShadow: '0px 0px 8px rgb(255,255,255)',
+    transition: {
+      repeat: Infinity,
+      repeatType: 'mirror',
+      duration: 0.4,
+    },
+  },
+};
 
 const Contact = () => (
   <div>
-    <h2 className="contact-comment">I&apos;m always interested in hearing about new projects, so if you&apos;d like to chat please get in touch.</h2>
+    <h1 className="about-title">Contact me</h1>
+    <h3 className="contact-comment">I&apos;m always interested in hearing about new projects, so if you&apos;d like to chat please get in touch.</h3>
     <form action="https://formspree.io/f/mqkoaqdk/" method="POST">
       <ul className="form-list">
         <li>
@@ -15,7 +30,14 @@ const Contact = () => (
           <textarea name="user-message" id="message" maxLength="500" required placeholder="Write me something..." />
         </li>
         <li className="contact-button">
-          <button className="form-submit" type="submit">Get in touch</button>
+          <motion.button
+            variants={buttonVariants}
+            whileHover="hover"
+            className="form-submit"
+            type="submit"
+          >
+            Get in touch
+          </motion.button>
         </li>
         <span id="error" aria-live="polite" />
       </ul>
