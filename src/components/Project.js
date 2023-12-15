860dbfa1bb2e376af8f793ee310e737f3bfa99e7
Sunga Thawethe <sungabt@gmail.com>
2023-12-15 22:50:43 +0200

    Add live button, add modal and useState, update classes

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { motion } from 'framer-motion';
 
@@ -16,35 +16,66 @@ const buttonVariants = {
 };
 
 const Project = ({
-  name, description, featuredImage, technologies, category,
-}) => (
-  <div className={`${category} 'project'`}>
-    <div>
-      <img src={featuredImage} alt="project" />
-    </div>
-    <h4>{name}</h4>
-    <p className="proj-desc">{description}</p>
-    <ul className="techs">
-      {technologies.map((tech) => (
-        <li className="tech" key={tech.id}>{tech}</li>
-      ))}
-    </ul>
-    <motion.button
-      variants={buttonVariants}
-      whileHover="hover"
-      type="button"
-    >
-      More Info
-    </motion.button>
-  </div>
-);
+  name, description, featuredImage, technologies, liveLink, sourceLink, category, deployed,
+}) => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const close = () => setModalOpen(false);
+  const open = () => setModalOpen(true);
+
+  return (
+    <>
+      {modalOpen}
+      <div className={`${category} 'project'`}>
+        <div className="img-div">
+          <img className="proj-img" src={featuredImage} alt="project" />
+        </div>
+        <h4>{name}</h4>
+        <p className="proj-desc">{description}</p>
+        <ul className="techs">
+          {technologies.map((tech) => (
+            <li className="tech" key={tech.id}>{tech}</li>
+          ))}
+        </ul>
+        <a href={sourceLink} target="_blank" rel="noreferrer">
+          <motion.button
+            variants={buttonVariants}
+            whileHover="hover"
+            whileTap={{ scale: 0.9 }}
+            onClick={() => (modalOpen ? close() : open())}
+          >
+            Source
+          </motion.button>
+        </a>
+        <span> </span>
+        {true
+        && (
+        <a href={liveLink} target="_blank" rel="noreferrer">
+          <motion.button
+            variants={buttonVariants}
+            whileHover="hover"
+            whileTap={{ scale: 0.9 }}
+            onClick={() => (modalOpen ? close() : open())}
+          >
+            {deployed}
+            Live
+          </motion.button>
+        </a>
+        )}
+      </div>
+    </>
+  );
+};
 
 Project.propTypes = {
   name: PropTypes.string,
   description: PropTypes.string,
   featuredImage: PropTypes.string,
   technologies: PropTypes.string,
+  liveLink: PropTypes.string,
+  sourceLink: PropTypes.string,
   category: PropTypes.string,
+  deployed: PropTypes.bool,
 };
 
 Project.defaultProps = {
@@ -52,7 +83,10 @@ Project.defaultProps = {
   featuredImage: '',
   description: '',
   technologies: '',
+  liveLink: '',
+  sourceLink: '',
   category: '',
+  deployed: '',
 };
 
 export default Project;
